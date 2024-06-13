b4df2531722008c0e3f4cbe0b69b464343ce29f0
Agustin Del valle <65829590+agusdelva@users.noreply.github.com>
2024-06-13 16:29:04 -0300

    Update 1A.sql

--- 1A.sql
+++ 1A.sql
@@ -1,8 +1,2 @@
---1- (A) de la tabla autos mostrar patente, marca, modelo, color, anio y precio de
---todo los autos, ordenados por precio ; (B) de los autos cuyo precio sea
---mayor a $10.500.000 ; (C) los autos cuyo precio este entre $9.000.000 y
---$14.000.000 ; (D) los autos marca “Peugeot “ y color “blanco” .
---(E) patente, marca, modelo y precio de los autos cuya marca no sea Ford ni
---Chevrolet ordenado por marca.
 select patente, marca, modelo, color, anio,precio from autos
 order by precio;
